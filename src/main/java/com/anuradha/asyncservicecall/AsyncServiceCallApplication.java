2a885557715f2d93a161eef3fc7dbdb72c7ea27c
Anuradha Ranasinghe <anuranasinghe151@gmail.com>
2023-01-17 12:16:52 +0530

    project initialization

---
+++ src/main/java/com/anuradha/asyncservicecall/AsyncServiceCallApplication.java
@@ -0,0 +1,13 @@
+package com.anuradha.asyncservicecall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncServiceCallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncServiceCallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anuradha/asyncservicecall/AsyncServiceCallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuradha.asyncservicecall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncServiceCallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
